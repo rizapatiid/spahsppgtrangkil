8f64d5cc7de7dc95b948b6c4612696c10d6c6d77
RIZA PROJECTS <rizapatiid@gmail.com>
2026-09-04 19:42:04 +0700

    chore: remove test file

--- test_div.ts
+++
@@ -1,9 +0,0 @@
-
-import { prisma } from "./src/lib/prisma"
-
-async function main() {
-  const divisi = await prisma.divisi.findMany()
-  console.log(divisi)
-}
-main().catch(console.error).finally(() => prisma.$disconnect())
-
